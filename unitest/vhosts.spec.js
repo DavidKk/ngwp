b92229636f074d7b3b523fd8cd7ad91aa5f99623
davidjones <qowera@qq.com>
2017-03-01 16:27:51 +0800

    add nginx config test

--- unitest/vhosts.spec.js
+++ unitest/vhosts.spec.js
@@ -6,6 +6,10 @@ import './vhosts/vhosts.helper';
 import _           from 'lodash';
 import fs          from 'fs-extra';
 import path        from 'path';
+import async       from 'async';
+import colors      from 'colors';
+import { exec }    from 'child_process';
+import handlebars  from 'handlebars';
 import { expect }  from 'chai';
 import { mkVhost } from '../src/libs/vhosts';
 import {
@@ -13,15 +17,15 @@ import {
   TEMPORARY_FOLDER_NAME,
 }                  from '../src/conf/config';
 
-describe('VHost Generator', function () {
+describe('Vhosts Generator', function () {
   let srcPath = path.join(__dirname, './vhosts');
   let tmpPath = path.join(EXEC_PATH, TEMPORARY_FOLDER_NAME, 'test_vhosts_' + Date.now());
 
-  after(function () {
-    fs.removeSync(tmpPath);
-  });
+  // after(function () {
+  //   fs.removeSync(tmpPath);
+  // });
 
-  describe('Test Generate', function () {
+  describe('Test Generating', function () {
     it('should configure and generate nginx config file', function (done) {
       let tplFile   = path.join(srcPath, 'vhost.conf.hbs');
       let outFile   = path.join(tmpPath, './vhosts.conf');
@@ -76,6 +80,82 @@ describe('VHost Generator', function () {
         expect(JSON.stringify(source.modules)).to.equal(JSON.stringify(modules));
 
         done();
+      });
+    });
+  });
+
+  describe('Test Running', function () {
+    it('can be used by nginx', function (done) {
+      let outFile   = path.join(tmpPath, './vhosts.conf');
+      let logFolder = path.join(tmpPath, './logs');
+      let appFolder = path.join(tmpPath, './apps');
+
+      let modules = [
+        {
+          type      : 'cdn',
+          domain    : ['cdn.example.com']
+        },
+        {
+          type      : 'proxy',
+          proxy     : '127.0.0.1',
+          proxyPort : 8080,
+          entries   : ['home'],
+          domain    : ['www.example.com'],
+        },
+      ];
+
+      let config = {
+        trace    : true,
+        distFile : outFile,
+        rootPath : appFolder,
+        logsPath : logFolder,
+
+        useHttps : true,
+        certPath : path.join(__dirname, './vhosts/certs'),
+        certFile : 'cert.pem',
+        certKey  : 'cert.key',
+      };
+
+      async.parallel([
+        function (callback) {
+          mkVhost(modules, config, function (error) {
+            expect(error).to.not.be.an('error');
+
+            callback(null, outFile);
+          });
+        },
+        function (callback) {
+          let ngxTplFile  = path.join(__dirname, './vhosts/nginx.conf.hbs');
+          let ngxOutFile  = path.join(tmpPath, 'nginx.conf');
+          let ngxTemplate = fs.readFileSync(ngxTplFile, 'utf-8');
+          let compile     = handlebars.compile(ngxTemplate);
+          let source      = compile({ files: [outFile] });
+
+          fs.writeFile(ngxOutFile, source, function (error) {
+            expect(error).to.not.be.an('error');
+
+            callback(null, ngxOutFile);
+          });
+        },
+      ],
+      /**
+       * this error must be a null and argument 2 must be a Array,
+       * if not `[outFile, ngxOutFile]` must be throw a error.
+       */
+      function (error, [outFile, ngxOutFile]) {
+        exec(`nginx -t -c ${ngxOutFile}`, function (error) {
+          if (/command not found/.exec(error.message)) {
+            /* eslint no-console:off */
+            console.log(colors.yellow('Nginx is not install, this unitest will pass'));
+
+            done();
+            return;
+          }
+
+          expect(new RegExp(`nginx: the configuration file ${ngxOutFile} syntax is ok`).test(error.message)).to.be.true;
+
+          done();
+        });
       });
     });
   });
